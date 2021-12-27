1c67343ff83d8e037826029ce2ca1a7ac18af3a8
Shane Oatman (HE/HIM) <shoatman@microsoft.com>
2021-12-27 10:35:30 -0800

    Updated method invocation

--- common4j/src/main/com/microsoft/identity/common/java/cache/MsalOAuth2TokenCache.java
+++ common4j/src/main/com/microsoft/identity/common/java/cache/MsalOAuth2TokenCache.java
@@ -911,6 +911,7 @@ public class MsalOAuth2TokenCache
                 accountRecord.getEnvironment(),
                 IdToken,
                 clientId, // If null, behaves as wildcard
+                null,
                 accountRecord.getRealm(),
                 null, // wildcard (*),
                 null, // not applicable
